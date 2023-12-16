1b76fcd6610470888b2c6ed7e8fa44665a033382
João Victor <35135930+S1LV4@users.noreply.github.com>
2023-12-16 10:34:39 -0300

    .

--- vendor/moment/moment-with-locales.js
+++ vendor/moment/moment-with-locales.js
@@ -14489,7 +14489,7 @@
         months : '一月_二月_三月_四月_五月_六月_七月_八月_九月_十月_十一月_十二月'.split('_'),
         monthsShort : '1月_2月_3月_4月_5月_6月_7月_8月_9月_10月_11月_12月'.split('_'),
         weekdays : '星期日_星期一_星期二_星期三_星期四_星期五_星期六'.split('_'),
-        weekdaysShort : '周日_周一_周二_周三_周四_周五_周六'.split('_'),
+        weekdaysShort : 'Semana日_Semana一_Semana二_Semana三_Semana四_Semana五_Semana六'.split('_'),
         weekdaysMin : '日_一_二_三_四_五_六'.split('_'),
         longDateFormat : {
             LT : 'HH:mm',
@@ -14542,7 +14542,7 @@
             lastWeek : '[上]ddddLT',
             sameElse : 'L'
         },
-        dayOfMonthOrdinalParse: /\d{1,2}(日|月|周)/,
+        dayOfMonthOrdinalParse: /\d{1,2}(日|月|Semana)/,
         ordinal : function (number, period) {
             switch (period) {
                 case 'd':
@@ -14553,7 +14553,7 @@
                     return number + '月';
                 case 'w':
                 case 'W':
-                    return number + '周';
+                    return number + 'Semana';
                 default:
                     return number;
             }
